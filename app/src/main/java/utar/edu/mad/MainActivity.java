fc32f22af683b6dbed2a549f177f6a4552b89e57
MADStudentUtar <newbieang2019@gmail.com>
2020-08-26 10:53:51 +0800

    Initial commit

---
+++ app/src/main/java/utar/edu/mad/MainActivity.java
@@ -0,0 +1,53 @@
+package utar.edu.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.setting_2_1);
+
+        mAuth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.signoutbutton1);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser == null)
+        {
+            Intent intent = new Intent(MainActivity.this,login.class);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+}
